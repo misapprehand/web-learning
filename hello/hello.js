183f3c9f23900d1d1b5d6dafdffc70aaf7cb8870
weiwu <weiwu@smilingmobile.com>
2017-09-07 13:33:05 +0800

    add hello function

--- hello/hello.js
+++ hello/hello.js
@@ -1,4 +1,13 @@
-var myHeading = document.querySelector('h2');
+//在标题栏显示hello
+var myHeading = document.querySelector('#hello_title');
 var old = myHeading.innerHTML;
 myHeading.innerHTML=old+'<p>Hello!</p>';
+
+//自定义函数
+var helloDiv = document.querySelector('#hello_div');
+helloDiv.innerHTML=hello();
+
+function hello(){
+    return 'hello';
+}
 
