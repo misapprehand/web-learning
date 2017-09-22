3a3200ac323a83ba7ced3323acc2ac05cd04c275
weiwu <weiwu@smilingmobile.com>
2017-09-22 09:36:12 +0800

    把页面入口都移到index.js

--- hello/src/index.js
+++ hello/src/index.js
@@ -1,8 +1,13 @@
 
 function entries(){
     const root = document.createElement('div');
-    root.innerHTML = '<p> <a href="src/html_element/html_element_category.html">HTML元素分类</a> </p>';
-
+    root.innerHTML = '<p> <a href="src/html_element/html_element_category.html">HTML元素分类</a>'
+                     +'</p><p><a href="src/html_element/html_element_attribute.html">HTML元素属性</a></p>'
+                     +'<p><a href="src/html_text/html_text.html">HTML文本</a></p>'
+                     +'<p><a href="src/html_image/html_image.html">HTML图片</a></p>'
+                     +'<p><a href="src/css_demo/css.html">CSS</a></p>'
+                     +'<p><a href="src/js_basic/js_basic.html">JS基础</a></p>'
+                     +'<p><a href="src/calc/calc.html">计算器</a></p>';
     return root;
 }
 document.body.appendChild(entries());
