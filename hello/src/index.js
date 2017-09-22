532f512f83696586ab67d9af92d86b6108ec786d
weiwu <weiwu@smilingmobile.com>
2017-09-22 17:00:34 +0800

    index.js分两列

--- hello/src/index.js
+++ hello/src/index.js
@@ -2,14 +2,18 @@ import createJsBaisc from './js_basic/hello';
 
 function entries(){
     const root = document.createElement('div');
-    root.innerHTML = '<p> <a href="src/html_element/html_element_category.html">HTML元素分类</a>'
+    root.innerHTML = '<div class="two-column">'
+                     +'<div>'
+                     +'<p> <a href="src/html_element/html_element_category.html">HTML元素分类</a>'
                      +'</p><p><a href="src/html_element/html_element_attribute.html">HTML元素属性</a></p>'
                      +'<p><a href="src/html_text/html_text.html">HTML文本</a></p>'
                      +'<p><a href="src/html_image/html_image.html">HTML图片</a></p>'
                      +'<p><a href="src/css_demo/css.html">CSS</a></p>'
                      +'<p id="js_basic_entry"><a href="#js_basic">JS基础</a></p>'
                      +'<p><a href="src/calc/calc.html">计算器</a></p>'
+                     +"</div>"
                      +'<div id="content"></div>'
+                     +'</div>'
     ;
     return root;
 }
