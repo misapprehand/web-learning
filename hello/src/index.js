a1fa9df34f2d1d5f5558eb05827bb311a3095d2f
weiwu <weiwu@smilingmobile.com>
2017-09-22 14:18:16 +0800

    Show js_basic in content div

--- hello/src/index.js
+++ hello/src/index.js
@@ -8,13 +8,16 @@ function entries(){
                      +'<p><a href="src/html_image/html_image.html">HTML图片</a></p>'
                      +'<p><a href="src/css_demo/css.html">CSS</a></p>'
                      +'<p id="js_basic_entry"><a href="#js_basic">JS基础</a></p>'
-                     +'<p><a href="src/calc/calc.html">计算器</a></p>';
+                     +'<p><a href="src/calc/calc.html">计算器</a></p>'
+                     +'<div id="content"></div>'
+    ;
     return root;
 }
 const root = entries();
 document.body.appendChild(root);
 
+const content = document.getElementById("content");
 const js_basic = document.getElementById("js_basic_entry");
 js_basic.onclick = function(){
-     createJsBaisc(root);
+     createJsBaisc(content);
 }
