f1506e3c8958313e9dc1da0aa569da66481980bb
weiwu <weiwu@smilingmobile.com>
2017-10-09 10:48:50 +0800

    add more test to input test

--- hello/src/calc/input.test.js
+++ hello/src/calc/input.test.js
@@ -1,4 +1,6 @@
-import * as input from './input';
+import Input from './input';
+
+const input = new Input({clearCallback:console.log("clear callback")});
 
 test('input 1 number and get 1',()=>{
     input.addNumber(1);
@@ -20,4 +22,21 @@ test('input 1, 2,+,1,1,= and 输出 [12,11]',()=>{
     input.addNumber(1);
     expect(input.getOperand()).toEqual([12,11]);
     input.clear();
+})
+test('input 1, 2,+,1,1,= 23',()=>{
+    input.addNumber(1);
+    input.addNumber(2);
+    input.addOperator('+');
+    input.addNumber(1);
+    input.addNumber(1);
+    const result = input.getResult();
+    expect(result).toEqual(23);
+    input.clear();
+})
+test('input 2*4= 8',()=>{
+    input.addNumber(2);
+    input.addOperator('*');
+    input.addNumber(4);
+    const result = input.getResult();
+    expect(result).toEqual(8);
 })
