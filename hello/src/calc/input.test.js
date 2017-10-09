ca153528f6b7154c4767339b7ff4fb14fbd3efe7
weiwu <weiwu@smilingmobile.com>
2017-10-09 10:53:48 +0800

    input.test add setup/teardown

--- hello/src/calc/input.test.js
+++ hello/src/calc/input.test.js
@@ -2,16 +2,21 @@ import Input from './input';
 
 const input = new Input({clearCallback:console.log("clear callback")});
 
+beforeEach(() => {
+    console.log("beforeEach");
+});
+
+afterEach(() => {
+    input.clear();
+});
 test('input 1 number and get 1',()=>{
     input.addNumber(1);
     expect(input.getOperand()).toContain(1);
-    input.clear();
 })
 test('input 1, 1 and get 11',()=>{
     input.addNumber(1);
     input.addNumber(1);
     expect(input.getOperand()).toContain(11);
-    input.clear();
 })
 test('input 1, 2,+,1,1,= and 输出 [12,11]',()=>{
     input.addNumber(1);
@@ -21,7 +26,6 @@ test('input 1, 2,+,1,1,= and 输出 [12,11]',()=>{
     input.addNumber(1);
     input.addNumber(1);
     expect(input.getOperand()).toEqual([12,11]);
-    input.clear();
 })
 test('input 1, 2,+,1,1,= 23',()=>{
     input.addNumber(1);
@@ -31,7 +35,6 @@ test('input 1, 2,+,1,1,= 23',()=>{
     input.addNumber(1);
     const result = input.getResult();
     expect(result).toEqual(23);
-    input.clear();
 })
 test('input 2*4= 8',()=>{
     input.addNumber(2);
