e570178c6a9bdc6569849dbe84ab2f9ddb7aea2f
weiwu <weiwu@smilingmobile.com>
2017-10-09 16:23:33 +0800

    创建 bootstrap readonly input demo

--- hello/src/bootstrap_demo/index.js
+++ hello/src/bootstrap_demo/index.js
@@ -8,11 +8,22 @@ function createButtonItem({title,classes}){
     element.textContent=title;
     return element;
 }
+function createReadOnlyInputItem(){
+    const element = document.createElement("input");
+    element.setAttribute("type","text")
+    element.setAttribute("class","form-control")
+    element.setAttribute("readonly","")
+    element.setAttribute('placeholder',"只读输入显示");
+
+    return element;
+}
 export default function createPage(content){
     content.innerHTML = '<h2>Bootstrap demo</h2>'
         +'<ul id="bs_demo_group">'
         +'</ul>';
     const group = document.getElementById("bs_demo_group");
-    const element = createButtonItem({title:"按钮:默认风格", classes:"btn btn-default"});
+    let element = createButtonItem({title:"按钮:默认风格", classes:"btn btn-default"});
+    addItem(group,element);
+    element = createReadOnlyInputItem();
     addItem(group,element);
 }
