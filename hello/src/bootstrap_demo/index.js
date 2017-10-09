1b1566f7146e16767f6cc3d34affd0580cd4b09e
weiwu <weiwu@smilingmobile.com>
2017-10-09 14:40:42 +0800

    修改bootstrap_demo页面，用js创建按钮

--- hello/src/bootstrap_demo/index.js
+++ hello/src/bootstrap_demo/index.js
@@ -1,14 +1,18 @@
+function addItem(parentNode,element){
+    parentNode.appendChild(element);  
+}
+function createButtonItem({title,classes}){
+    const element = document.createElement('button');
+    element.setAttribute("type","button");
+    element.setAttribute("class",classes);
+    element.textContent=title;
+    return element;
+}
 export default function createPage(content){
     content.innerHTML = '<h2>Bootstrap demo</h2>'
-        +'<ul>'
-        +'<li>'
-        +'按钮'
-        +'<ul>'
-        +'<li>'
-        +'<button type="button" class="btn btn-default">默认风格</button>'
-        +'</li>'
-        +'<ul>'
-        +'</li>'
-        +'</ul>'
-    ;
+        +'<ul id="bs_demo_group">'
+        +'</ul>';
+    const group = document.getElementById("bs_demo_group");
+    const element = createButtonItem({title:"按钮:默认风格", classes:"btn btn-default"});
+    addItem(group,element);
 }
