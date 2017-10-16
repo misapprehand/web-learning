9c40aa5b64c38ed217550fcfe8a5f4e71315e6a6
weiwu <weiwu@smilingmobile.com>
2017-10-16 14:49:37 +0800

    Move more code to SideBar and clean index.js

--- hello/src/containers/sideBar.js
+++ hello/src/containers/sideBar.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react';
 
+const items = [
+  {
+    'id': 'htmlCategory',
+    'href': 'src/html_element/html_element_category.html',
+    'text': 'HTML元素分类'
+  },
+  {
+    'id': 'htmlAttribute',
+    'href': 'src/html_element/html_element_attribute.html',
+    'text': 'HTML元素属性'
+  },
+  {
+    'id': 'htmlText',
+    'href': 'src/html_text/html_text.html',
+    'text': 'HTML文本'
+  },
+  {
+    'id': 'htmlImage',
+    'href': 'src/html_image/html_image.html',
+    'text': 'HTML图片'
+  },
+  {
+    'id': 'cssDemo',
+    'href': 'src/css_demo/html_image.html',
+    'text': 'CSS'
+  },
+  {
+    'id': 'js_basic_entry',
+    'href': '#js_basic',
+    'text': 'JS基础'
+  },
+  {
+    'id': 'calc_entry',
+    'href': '#calc',
+    'text': '计算器'
+  },
+  {
+    'id': 'bootstrap_entry',
+    'href': '#bootstrap',
+    'text': 'Bootstrap Demo'
+  }
+];
+function NavSideBar (props) {
+  const navItems = (items) => {
+    const navItems = [];
+    for (let item of items) {
+      navItems.push(<p id={item.id} key={item.id}><a href={item.href}>{item.text}</a></p>);
+    }
+    return navItems;
+  };
+  return (<nav>{ navItems(props.items) }</nav>);
+}
+
 class SideBar extends Component {
   render () {
-    return <nav>{this.props.items}</nav>;
+    return <NavSideBar items={items} />;
   }
 }
 export default SideBar;
--- hello/src/index.js
+++ hello/src/index.js
@@ -5,62 +5,10 @@ import createCalc from './calc/calc';
 import createBootstrap from './bootstrap_demo/index';
 import SideBar from './containers/sideBar';
 
-function NavSideBar (props) {
-  return <nav>{ props.items }</nav>;
-}
 function entries () {
-  const items = [
-    {
-      'id': 'htmlCategory',
-      'href': 'src/html_element/html_element_category.html',
-      'text': 'HTML元素分类'
-    },
-    {
-      'id': 'htmlAttribute',
-      'href': 'src/html_element/html_element_attribute.html',
-      'text': 'HTML元素属性'
-    },
-    {
-      'id': 'htmlText',
-      'href': 'src/html_text/html_text.html',
-      'text': 'HTML文本'
-    },
-    {
-      'id': 'htmlImage',
-      'href': 'src/html_image/html_image.html',
-      'text': 'HTML图片'
-    },
-    {
-      'id': 'cssDemo',
-      'href': 'src/css_demo/html_image.html',
-      'text': 'CSS'
-    },
-    {
-      'id': 'js_basic_entry',
-      'href': '#js_basic',
-      'text': 'JS基础'
-    },
-    {
-      'id': 'calc_entry',
-      'href': '#calc',
-      'text': '计算器'
-    },
-    {
-      'id': 'bootstrap_entry',
-      'href': '#bootstrap',
-      'text': 'Bootstrap Demo'
-    }
-  ];
-  const navItems = (items) => {
-    const navItems = [];
-    for (let item of items) {
-      navItems.push(<p id={item.id} key={item.id}><a href={item.href}>{item.text}</a></p>);
-    }
-    return navItems;
-  };
   return (
     <div id='main'>
-      <SideBar items={navItems(items)} />
+      <SideBar />
       <article id='content' />
     </div>
   );
