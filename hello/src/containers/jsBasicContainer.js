f602f56f45d839d67a6bd336c5438318e1702646
weiwu <weiwu@smilingmobile.com>
2017-10-18 15:27:55 +0800

    改写Hello.js功能到 React Component-1
    1. createItem 部分

--- hello/src/containers/jsBasicContainer.js
+++ hello/src/containers/jsBasicContainer.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 class JsBasicContainer extends Component {
   render () {
     return (
-      <div>JSBasic</div>
+      <div>
+        <h2 id='hello_title'>Hello 标题</h2>
+        <h2>自定义函数</h2>
+        <div id='hello_div' />
+        <h2>自定义函数2</h2>
+        <div id='mul_div' />
+        <h2>点按钮，显示数字</h2>
+        <div id='number_btn_div'>
+          <input type='button' id='1' value='1' />
+          <input type='button' id='2' value='2' />
+          <div id='result' />
+        </div>
+      </div>
     );
   }
 }
