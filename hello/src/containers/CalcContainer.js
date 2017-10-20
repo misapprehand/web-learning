4b0014d2ea81c85c800a04e9099a165054730bd1
weiwu <weiwu@smilingmobile.com>
2017-10-20 10:57:44 +0800

    change class to className

--- hello/src/containers/CalcContainer.js
+++ hello/src/containers/CalcContainer.js
@@ -6,7 +6,7 @@ class CalcContainer extends Component {
       <div>CalcContainer
         <h2>简单乘法器</h2>
         <div id='multiplier'>
-          <input id='mul-result' type='text' class='form-control' readonly />
+          <input id='mul-result' type='text' className='form-control' readonly />
         </div>
       </div>
     );
